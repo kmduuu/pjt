72daf34f8877652a27fe4ccf02dd7948a131d052
Bitcamp <Bitcamp@DESKTOP-KESVINK>
2023-05-17 13:29:27 +0900

    Create : 23.05.17 13:29

---
+++ ZZproject/src/test/java/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+﻿package spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
